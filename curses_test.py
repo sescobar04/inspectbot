5ef86b6b8fc53844795f8535556be3add115acd1
Seth Escobar <seth.escobar@students.tamuk.edu>
2020-06-18 18:16:18 +0100

    Added curses_test.py to test wireless keyboard viability as a controller

---
+++ curses_test.py
@@ -0,0 +1,36 @@
+#Code taken from https://explainingcomputers.com/sample_code/curses_key_test.py
+#Used to test the wireless keyboard I will be using to control the bot
+
+# import curses
+import curses
+
+# Get the curses window, turn off echoing of keyboard to screen, turn on
+# instant (no waiting) key response, and use special values for cursor keys
+screen = curses.initscr()
+#curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+try:
+        while True:
+            char = screen.getch()
+            if char == ord('q'):
+                print("q pressed")
+		break
+            elif char == curses.KEY_UP:
+                print("up")
+	    elif char == curses.KEY_F1: #Seems like this module will work for what I have in mind
+		print("F1")
+            elif char == curses.KEY_DOWN:
+                print("down")
+            elif char == curses.KEY_RIGHT:
+                print("right")
+            elif char == curses.KEY_LEFT:
+                print("left")
+            elif char == 10:
+                print("stop")
+finally:
+    #Close down curses properly, inc turn echo back on!
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+
