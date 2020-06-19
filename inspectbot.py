5cacc8ff63f8aa1a033c5f5a6e2608c51451b7d5
Seth Escobar <seth.escobar@students.tamuk.edu>
2020-06-19 19:58:55 +0100

    Moved the motor functions and lists containing the GPIO pin numbers to Robot class

--- inspectbot.py
+++ inspectbot.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO # For controlling the raspberry pi's GPIO pins
-import curses # For keyboard input to control robot
 import datetime # For testing time passed in time_check(). Necessary for dead man switch
 from time import sleep # Necessary for LED_flash and testing time_check() and by extension, dead_man()
 
@@ -8,53 +7,6 @@ def gpio_setup_outputs(pins):
 	for pin in pins:
 		GPIO.setup(pin, GPIO.OUT)
 	return;
-
-def LED_on(pin):
-	# Turns on a specified LED
-	#pin is the number of the GPIO pin to be turned on
-	GPIO.output(pin, True)
-	print("LED on")
-
-def LED_off(pin):
-	# Turns off a specified LED
-	#pin is the number of the GPIO pin to be turned off
-	GPIO.output(pin, False)
-	print("LED off")
-
-def LED_flash(pin):
-	# Flashes a specified LED
-	#pin is the number of the GPIO pin to be flashed
-	for i in range(0, 2):
-		LED_on(pin)
-		sleep(.500)
-		LED_off(pin)
-		sleep(.500)
-
-def motors_stop(l_motor_pins, r_motor_pins):
-	#Stop both motors regardless of direction
-	for pin in l_motor_pins: #Stopping left motor
-		GPIO.output(pin, False)
-	for pin in r_motor_pins: #Stopping right motor
-		GPIO.output(pin, False)
-	print("Stop motors")
-
-def motor_forward(motor_pins):
-	# Sets specified motor to move forward
-	GPIO.output(motor_pins[1], False) # Ensures the motor isn't also trying to reverse
-	GPIO.output(motor_pins[0], True) # Sets the motor to move forward
-	print("motor_forward, pin: " + str(motor_pins[0]))
-
-def motor_reverse(motor_pins):
-	# Sets specified motor to reverse
-	GPIO.output(motor_pins[0], False) # Ensures the motor isn't also trying to move forward
-	GPIO.output(motor_pins[1], True) # Sets the motor to reverse
-	print("motor_reverse, pin: " + str(motor_pins[1]))
-
-def motor_set_speed(motor_pins, speed):
-	motor_speed = []
-	for pin in motor_pins:
-		motor_speed.append(GPIO.PWM(pin, speed))
-	return motor_speed
 
 def time_check(last_time):
 	# Time check will be used to test if a specified amount of time has passed
@@ -76,31 +28,25 @@ def dead_man(last_time, l_motor_pins, r_motor_pins):
 		motors_stop(l_motor_pins, r_motor_pins) # Stop both motors
 		return(True)
 
-def test_functions(l_motor_pins, r_motor_pins, LEDs):
+def test_functions(Robot):
 	print("TEST FUNCTIONS")
-	motor_forward(l_motor_pins)
-	motor_reverse(r_motor_pins)
+	Robot.motor_forward(Robot.left_motor)
+	Robot.motor_reverse(Robot.right_motor)
 	sleep(1)
-	motor_forward(r_motor_pins)
-	motor_reverse(l_motor_pins)
+	Robot.motor_forward(Robot.right_motor)
+	Robot.motor_reverse(Robot.left_motor)
 	sleep(1)
-	motor_forward(l_motor_pins)
-	motor_forward(r_motor_pins)
+	Robot.motor_forward(Robot.left_motor)
+	Robot.motor_forward(Robot.right_motor)
 	sleep(1)
-	motor_reverse(l_motor_pins)
-	motor_reverse(r_motor_pins)
+	Robot.motor_reverse(Robot.left_motor)
+	Robot.motor_reverse(Robot.right_motor)
 	sleep(1)
-	motors_stop(l_motor_pins, r_motor_pins)
+	Robot.motors_stop
 	sleep(.500)
-	last_time = datetime.datetime.now()
-	motor_forward(l_motor_pins)
-	motor_reverse(r_motor_pins)
-	for i in range(4):
-		sleep(1)
-		dead = dead_man(last_time, l_motor_pins, r_motor_pins)
-		if dead:
-			break
-	LED_flash(LEDs[0])
+	Robot.motor_forward(Robot.left_motor)
+	Robot.motor_reverse(Robot.right_motor)
+	Robot.LED_flash(Robot.LEDs[0])
 	return(1)
 
 def pilot_control(input, l_motor_pins, r_motor_pins):
@@ -125,18 +71,82 @@ def pilot_control(input, l_motor_pins, r_motor_pins):
 		time = datetime.datetime.now()
 	return(time)
 
+
+class Robot:
+	#Contructor
+	def __init__(self, left_motor, right_motor, LEDs):
+		self.left_motor = left_motor
+		self.right_motor = right_motor
+		self.LEDs = LEDs
+
+	def LED_on(self, pin):
+		# Turns on a specified LED
+		#pin is the number of the GPIO pin to be turned on
+		GPIO.output(pin, True)
+		print("LED on")
+
+	def LED_off(self, pin):
+		# Turns off a specified LED
+		#pin is the number of the GPIO pin to be turned off
+		GPIO.output(pin, False)
+		print("LED off")
+
+	def LED_flash(self, pin):
+		# Flashes a specified LED
+		#pin is the number of the GPIO pin to be flashed
+		for i in range(0, 2):
+			self.LED_on(pin)
+			sleep(.500)
+			self.LED_off(pin)
+			sleep(.500)
+
+	def motors_stop(self):
+		#Stop both motors regardless of direction
+		for pin in self.left_motor: #Stopping left motor
+			GPIO.output(pin, False)
+		for pin in self.right_motor: #Stopping right motor
+			GPIO.output(pin, False)
+		print("Stop motors")
+
+	def motor_forward(self, motor_pins):
+		# Sets specified motor to move forward
+		GPIO.output(motor_pins[1], False) # Ensures the motor isn't also trying to reverse
+		GPIO.output(motor_pins[0], True) # Sets the motor to move forward
+		print("motor_forward, pin: " + str(motor_pins[0]))
+
+	def motor_reverse(self, motor_pins):
+		# Sets specified motor to reverse
+		GPIO.output(motor_pins[0], False) # Ensures the motor isn't also trying to move forward
+		GPIO.output(motor_pins[1], True) # Sets the motor to reverse
+		print("motor_reverse, pin: " + str(motor_pins[1]))
+
+	def motor_set_speed(motor_pins, speed):
+		motor_speed = []
+		for pin in motor_pins:
+			motor_speed.append(GPIO.PWM(pin, speed))
+		return motor_speed
+
+
 #main code execution
 try:
 
-	left_motor = [4, 17] # List of the GPIO pins that control the left motor
-	right_motor = [27, 22] # List of the GPIO pins that control the right motor
-	LED_pins = [23] # List of the GPIO pins that control the LEDS
+	#left_motor = [4, 17] # List of the GPIO pins that control the left motor
+	#right_motor = [27, 22] # List of the GPIO pins that control the right motor
+	#LED_pins = [23] # List of the GPIO pins that control the LEDS
+
+	inspectbot = Robot([4, 17], [27, 22], [23])
+
+	print(inspectbot.left_motor)
+	print(inspectbot.right_motor)
+	print(inspectbot.LEDs)
 
 	GPIO.setmode(GPIO.BCM) # Set the pin reference mode
-	gpio_setup_outputs(left_motor) # Set up the left motor pins
-	gpio_setup_outputs(right_motor) # Set up the right motor pins
-	motors_stop(left_motor, right_motor) # Stops both motors
-	gpio_setup_outputs(LED_pins) # Set up the LED GPIO pins
+	gpio_setup_outputs(inspectbot.left_motor) # Set up the left motor pins
+	gpio_setup_outputs(inspectbot.right_motor) # Set up the right motor pins
+	inspectbot.motors_stop() # Stops both motors
+	gpio_setup_outputs(inspectbot.LEDs) # Set up the LED GPIO pins
+
+
 
 	"""
 	I think for now it's best to leave out the attempt at PWM
@@ -150,14 +160,13 @@ try:
 	 print(right_motor_speed) 
 	"""
 
+	#screen = curses.initscr()
+	#curses.noecho()
+	#curses.cbreak()
+	#screen.keypad(True)
 
-	screen = curses.initscr()
-	curses.noecho()
-	curses.cbreak()
-	screen.keypad(True)
 
-	last_input = datetime.datetime.now()
-
+	"""
 	while True:
 		# current implementation of dead_man() will not work because screen.getch() pauses the while loop
 		# This means that dead_man() only checks how much time has passed when a key has been pressed
@@ -168,23 +177,17 @@ try:
 			break
 		else:
 			last_input = pilot_control(input, left_motor, right_motor)
-		#dead_man(last_input, left_motor, right_motor)
-	# test_functions(left_motor, right_motor, LED_pins)
-
-	print("Code works") # Only prints if all code runs
-
-	""" 
-	TO DO
-	 Add PWM speed control for motors
-	 Allow two or three selectable speeds for motors
-	 Purpose of this is to allow pilot finer resolution movement control 
 	"""
 
-finally:
-	curses.nocbreak(); screen.keypad(0); curses.echo()
-	curses.endwin()
 
-	motors_stop(left_motor, right_motor) # Stops motors once code is completed
-	LED_off(LED_pins) # Turns off all LEDs once code is completed
+	test_functions(inspectbot)
+	print("Code works") # Only prints if all code runs
+
+finally:
+	#curses.nocbreak(); screen.keypad(0); curses.echo()
+	#curses.endwin()
+
+	inspectbot.motors_stop() # Stops motors once code is completed
+	inspectbot.LED_off(inspectbot.LEDs) # Turns off all LEDs once code is completed
 	GPIO.cleanup() # Cleans up GPIO after code is completed
 	print("Code Finished") # Prints once execution is complete
